7f5a52c2d3705edbb149f80a55ccea575f3be951
Jan Schumann <js@schumann-it.com>
2021-03-30 08:43:21 +0200

    clone to TERRAFORM_MAKE_LIB_HOME

--- install.mk
+++ install.mk
@@ -2,12 +2,14 @@ TF_MAKE_BRANCH ?= master
 TF_MAKE_CLONE_URL ?= https://github.com/janschumann/terraform-make.git
 TF_MAKE_PATH ?= .terraform-make
 
--include $(TF_MAKE_PATH)/terraform.mk
+TERRAFORM_MAKE_LIB_HOME := $(shell pwd)/$(TF_MAKE_PATH)
+
+-include $(TERRAFORM_MAKE_LIB_HOME)/terraform.mk
 
 .PHONY : install
 install::
-	@ git clone -c advice.detachedHead=false --depth=1 -b $(TF_MAKE_BRANCH) $(TF_MAKE_CLONE_URL) $(TF_MAKE_PATH)
+	@git clone -c advice.detachedHead=false --depth=1 -b $(TF_MAKE_BRANCH) $(TF_MAKE_CLONE_URL) $(TERRAFORM_MAKE_LIB_HOME)
 
 .PHONY : clean
 clean::
-	@ rm -rf $(TF_MAKE_PATH)
+	@ rm -rf $(TERRAFORM_MAKE_LIB_HOME)
